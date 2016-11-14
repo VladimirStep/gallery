ad4a619b3430636a2680dced43ef94b721116148
Vladimir Stepanov <vladimir.stepanov@faceit-team.com>
2016-11-14 17:35:08 +0200

    Add model Picture and UploaderImage from carrierwave gem. Include carrierwave gem. Build associations.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ApplicationRecord
+  has_many :pictures, dependent: :destroy
   validates :category_name, presence: true, length: { maximum: 20 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161114124343) do
+ActiveRecord::Schema.define(version: 20161114145113) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "category_name"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+  end
+
+  create_table "pictures", force: :cascade do |t|
+    t.string   "picture_name"
+    t.integer  "category_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.string   "image"
+    t.index ["category_id"], name: "index_pictures_on_category_id"
   end
 
 end
