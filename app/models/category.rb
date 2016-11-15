283fcb7c4d747e8d5f096eae2f34c6a62966531f
Vladimir Stepanov <vladimir.stepanov@faceit-team.com>
2016-11-15 15:42:22 +0200

    Generate Categories conntroller and views

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,9 @@
 class Category < ApplicationRecord
   has_many :pictures, dependent: :destroy
-  validates :category_name, presence: true, length: { maximum: 20 }
+  validates :category_name, presence: true,
+                            length: { maximum: 20 },
+                            uniqueness: true
+  validates_associated :pictures
+
+  # TODO Capitalize categories name
 end
--- app/models/picture.rb
+++ app/models/picture.rb
@@ -2,4 +2,6 @@ class Picture < ApplicationRecord
   mount_uploader :image, ImageUploader
   belongs_to :category
   validates :picture_name, presence: true
+
+  # TODO Validate image field
 end
