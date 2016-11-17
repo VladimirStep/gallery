6ca39344968ef365edf1b982f14e97824b9cce3b
Vladimir Stepanov <vladimir.stepanov@faceit-team.com>
2016-11-17 12:12:03 +0200

    Add model Comment woth polimorphic associations to Uset and Picture

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,6 +1,7 @@
 class Picture < ApplicationRecord
   mount_uploader :image, ImageUploader
   belongs_to :category
+  has_many :comments, as: :commentable, dependent: :destroy
   validates :picture_name, presence: true
 
   # TODO Validate image field
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161116133842) do
+ActiveRecord::Schema.define(version: 20161117100054) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "category_name"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "body"
+    t.string   "commentable_type"
+    t.integer  "commentable_id"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+    t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable_type_and_commentable_id"
   end
 
   create_table "pictures", force: :cascade do |t|
