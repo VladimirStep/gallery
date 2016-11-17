8bc7133fbe1140bb35cde1b65b95b8f5c68e155a
Vladimir Stepanov <vladimir.stepanov@faceit-team.com>
2016-11-17 16:23:55 +0200

    Replace polymorphic association on Comment with has_many belongs_to

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,7 +1,7 @@
 class Picture < ApplicationRecord
   mount_uploader :image, ImageUploader
   belongs_to :category
-  has_many :comments, as: :commentable, dependent: :destroy
+  has_many :comments, dependent: :destroy
   validates :picture_name, presence: true
 
   # TODO Validate image field
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161117100054) do
+ActiveRecord::Schema.define(version: 20161117141737) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "category_name"
@@ -20,11 +20,12 @@ ActiveRecord::Schema.define(version: 20161117100054) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "body"
-    t.string   "commentable_type"
-    t.integer  "commentable_id"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
-    t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable_type_and_commentable_id"
+    t.integer  "user_id"
+    t.integer  "picture_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["picture_id"], name: "index_comments_on_picture_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "pictures", force: :cascade do |t|
