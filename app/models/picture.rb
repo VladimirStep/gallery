a1ba0c84a748518f994a9a07a367dc6e0abc8deb
Vladimir Stepanov <vladimir.stepanov@faceit-team.com>
2016-11-18 10:54:35 +0200

    Add model Like.

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -2,6 +2,7 @@ class Picture < ApplicationRecord
   mount_uploader :image, ImageUploader
   belongs_to :category
   has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
   validates :picture_name, presence: true
 
   # TODO Validate image field
