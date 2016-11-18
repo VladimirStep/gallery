3d22a585368663e58363c019ff31103755d457ae
Vladimir Stepanov <vladimir.stepanov@faceit-team.com>
2016-11-18 13:45:50 +0200

    Add Like/Dislike functionality

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,7 +1,25 @@
 class LikesController < ApplicationController
   def create
+    current_user.likes.create!(like_params)
+    redirect_to category_picture_path(category_param[:category_name], picture_param[:picture_id])
   end
 
   def destroy
+    Like.find(params[:id]).destroy
+    redirect_to category_picture_path(category_param[:category_name], picture_param[:picture_id])
+  end
+
+  private
+
+  def like_params
+    params.require(:like).permit(:picture_id)
+  end
+
+  def category_param
+    params.require(:like).permit(:category_name)
+  end
+
+  def picture_param
+    params.require(:like).permit(:picture_id)
   end
 end
