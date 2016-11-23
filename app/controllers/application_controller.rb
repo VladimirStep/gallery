dfec55fc28b99beecfb65aa77e342af24cbc4093
Vladimir Stepanov <vladimir.stepanov@faceit-team.com>
2016-11-23 18:07:41 +0200

    Configure l18n localization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  before_action :set_locale
   before_action :track_user
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  # OPTIMIZE Take locale params from routes
+  def default_url_options
+    { locale: I18n.locale }
+  end
 
   def after_sign_in_path_for(resource)
     stored_location_for(resource) || categories_path
