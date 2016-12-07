749d7d8edb8abb71edf875bc4082f888e6374587
Vladimir Stepanov <vladimir.stepanov@faceit-team.com>
2016-12-07 18:04:22 +0200

    Add Ajax to like action on picture show page

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,13 +2,27 @@ class LikesController < ApplicationController
   before_action :authenticate_user!
 
   def create
+    @category = Category.find_by(category_name: category_param[:category_name])
+    @picture = Picture.find_by(id: picture_param[:picture_id], category_id: @category.id)
+    @like = @picture.likes.where(user_id: current_user.id)
+
     current_user.likes.create!(like_params)
-    redirect_to category_picture_path(category_param[:category_name], picture_param[:picture_id])
+    respond_to do |format|
+      format.html { redirect_to category_picture_path(category_param[:category_name], picture_param[:picture_id]) }
+      format.js
+    end
   end
 
   def destroy
+    @category = Category.find_by(category_name: category_param[:category_name])
+    @picture = Picture.find_by(id: picture_param[:picture_id], category_id: @category.id)
+    @like = @picture.likes.where(user_id: current_user.id)
+
     Like.find(params[:id]).destroy
-    redirect_to category_picture_path(category_param[:category_name], picture_param[:picture_id])
+    respond_to do |format|
+      format.html { redirect_to category_picture_path(category_param[:category_name], picture_param[:picture_id]) }
+      format.js
+    end
   end
 
   private
