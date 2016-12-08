181bada1eea34367b8cb0bbc5418a2cd74b36bda
Vladimir Stepanov <vladimir.stepanov@faceit-team.com>
2016-12-08 12:48:35 +0200

    Bugfix for subscriptions views

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -6,6 +6,6 @@ class CategoriesController < ApplicationController
   def show
     @category = Category.find_by(category_name: params[:category_name])
     @pictures = @category.pictures.page(params[:page]).per(5)
-    @subscription = Subscription.find_by(category_id: @category.id, user_id: current_user.id)
+    @subscription = Subscription.find_by(category_id: @category.id, user_id: current_user.id) if user_signed_in?
   end
 end
--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -8,7 +8,7 @@ class PicturesController < ApplicationController
   def show
     @category = Category.find_by(category_name: params[:category_category_name])
     @picture = Picture.find_by(id: params[:id], category_id: @category.id)
-    @like = Like.find_by(picture_id: params[:id], user_id: current_user.id)
+    @like = Like.find_by(picture_id: params[:id], user_id: current_user.id) if user_signed_in?
     @comments = @picture.comments.order(created_at: :desc).page(params[:page]).per(5)
   end
 end
