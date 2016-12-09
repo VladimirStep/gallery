1414ad43fa2e784d364a08acbd1908e9d8c95ad6
Vladimir Stepanov <vladimir.stepanov@faceit-team.com>
2016-12-09 11:41:13 +0200

    Add mailler on after create Picture function

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -3,4 +3,12 @@ class Picture < ApplicationRecord
   belongs_to :category
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+
+  after_create :send_email_to_subscribers
+
+  def send_email_to_subscribers
+    @url = url_for(self.image)
+    # TODO It should return list of subscribers
+    # @subscribers = self.category.users
+  end
 end
