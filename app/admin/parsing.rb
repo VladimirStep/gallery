764968e28208051e907060c1eea4dcc703b42e84
Vladimir Stepanov <vladimir.stepanov@faceit-team.com>
2016-12-21 15:02:19 +0200

    Refactor parsing page

--- app/admin/parsing.rb
+++ app/admin/parsing.rb
@@ -2,46 +2,27 @@ ActiveAdmin.register_page 'Parsing' do
   menu priority: 3
 
   sidebar :parsing do
-    form_for :parsing do |f|
-      f.label 'Site url'
+    form_for :site, action: admin_parsing_result_path do |f|
+      f.label 'Site Url'
       f.text_field :url
       br
       br
-      f.submit 'Parse'
+      f.submit 'Pars'
     end
   end
-
-  # controller do
-  #   def index
-  #     if request.post?
-  #       @url = params[:url]
-  #
-  #     else
-  #       @url = 'Hello!'
-  #     end
-  #     @url = params[:url]
-  #   end
-  # end
-
 
   content do
     h3 'Enter site url for parsing'
 
-    div @url
   end
 
   action_item :view_site do
     link_to "View Site", "/"
   end
 
-  # action_item :add do
-  #   link_to "Add Event", admin_parsing_add_event_path, method: :post
-  # end
-
-
-  page_action :index, method: :post do
+  page_action :result, method: :post do
     # ...
-    @url = params[:url]
+    # @url = params[:url]
     # redirect_to admin_parsing_path, notice: "Your event was added"
   end
 
