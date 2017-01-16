a0ff7de3461782c9af5c31bc9909f1cd9dee4eff
Vladimir Stepanov <vladimir.stepanov@faceit-team.com>
2017-01-16 11:43:33 +0200

    Add tests for Likes controller

--- spec/controllers/likes_controller_spec.rb
+++ spec/controllers/likes_controller_spec.rb
@@ -1,19 +1,58 @@
 require 'rails_helper'
 
 RSpec.describe LikesController, type: :controller do
+  before do
+    @user = create(:user)
+    @picture = create(:picture)
+  end
 
-  describe "GET #enlike" do
-    it "returns http success" do
-      get :enlike
-      expect(response).to have_http_status(:success)
+  describe 'POST #create' do
+    context 'when user logged in' do
+      it 'creates new like' do
+        sign_in @user
+        request.headers['REQUEST_PATH'] = likes_path(@picture)
+        expect do
+          post :create, xhr: true, params: { user_id: @user.id, picture_id: @picture.id }
+        end.to change(@picture.likes, :count).by(1)
+        expect(response).to have_http_status(:success)
+        expect(response.content_type).to eq 'text/javascript'
+      end
+    end
+
+    context 'when user is not logged in' do
+      it 'does not create new like' do
+        expect do
+          post :create, xhr: true, params: { user_id: @user.id, picture_id: @picture.id }
+        end.not_to change(@picture.likes, :count)
+        expect(response.status).to eq 401
+        expect(response.content_type).to eq 'text/javascript'
+      end
     end
   end
 
-  describe "GET #dislike" do
-    it "returns http success" do
-      get :dislike
-      expect(response).to have_http_status(:success)
+  describe 'DELETE #destroy' do
+    context 'when user logged in' do
+      it 'deletes like' do
+        @like = create(:like, picture: @picture, user: @user)
+        sign_in @user
+        request.headers['REQUEST_PATH'] = like_path(@picture, @like)
+        expect do
+          delete :destroy, xhr: true, params: { picture_id: @picture.id, id: @like.id }
+        end.to change(@picture.likes, :count).by(-1)
+        expect(response).to have_http_status(:success)
+        expect(response.content_type).to eq 'text/javascript'
+      end
+    end
+
+    context 'when user is not logged in' do
+      it 'does not delete like' do
+        @like = create(:like, picture: @picture, user: @user)
+        expect do
+          delete :destroy, xhr: true, params: { picture_id: @picture.id, id: @like.id }
+        end.not_to change(@picture.likes, :count)
+        expect(response.status).to eq 401
+        expect(response.content_type).to eq 'text/javascript'
+      end
     end
   end
-
 end
