d49997bf753fb4ac6d656a27be430a5bb1d9dbb0
Vladimir Stepanov <vladimir.stepanov@faceit-team.com>
2017-01-20 11:55:00 +0200

    Add tests for like form on show page. Start testing comments on show page

--- spec/features/pictures_pages_spec.rb
+++ spec/features/pictures_pages_spec.rb
@@ -30,9 +30,7 @@ RSpec.feature 'Pictures pages', :type => :feature do
 
       it { should have_css('div.alert.alert-danger') }
       it { should have_content('You need to sign in or sign up before continuing.') }
-      it { should_not have_css("img[src*='#{picture.image_identifier}']") }
-      it { should_not have_css('div#like-form') }
-      it { should_not have_css('div#comment-form') }
+      it { should have_current_path(new_user_session_path(locale: :en)) }
     end
 
     context 'when user is logged in' do
@@ -58,16 +56,64 @@ RSpec.feature 'Pictures pages', :type => :feature do
     end
 
     it 'should open/close modal window', js: true do
-      expect(page).not_to have_css('body.modal-open')
-      expect(page).to have_css('div#myModal', visible: false)
+      expect(page).to have_no_css('body.modal-open')
+      expect(page).to have_no_css('div#myModal', visible: true)
 
       find_link(class: 'thumbnail', href: '#').click
       expect(page).to have_css('body.modal-open')
       expect(page).to have_css('div#myModal', visible: true)
 
       click_on('Close')
-      expect(page).not_to have_css('body.modal-open')
-      expect(page).to have_css('div#myModal', visible: false)
+      expect(page).to have_no_css('body.modal-open')
+      expect(page).to have_no_css('div#myModal', visible: true)
+    end
+  end
+
+  describe 'Like form on Show page' do
+    let(:picture) { create(:picture, category: category) }
+
+    before do
+      sign_in user
+      visit category_picture_path(category_category_name: category.category_name, id: picture.id)
+    end
+
+    it 'User can create/delete like on the picture', js: true do
+      expect(page).to have_css("input[src*='like_empty-']")
+      expect(page).to have_content('0 likes')
+      expect(picture.likes.count).to eq(0)
+      within('#like-form') do
+        click_on('Submit')
+      end
+      expect(page).to have_css("input[src*='like-']")
+      expect(page).to have_content('1 like')
+      expect(picture.likes.count).to eq(1)
+      within('#like-form') do
+        click_on('Submit')
+      end
+      expect(page).to have_css("input[src*='like_empty-']")
+      expect(page).to have_content('0 likes')
+      expect(picture.likes.count).to eq(0)
+    end
+  end
+
+  describe 'Comment form on Show page' do
+    let(:picture) { create(:picture, category: category) }
+
+    before do
+      sign_in user
+      visit category_picture_path(category_category_name: category.category_name, id: picture.id)
+    end
+
+    it 'User can leave comment on the picture', js: true do
+      expect(page).to have_content('0 COMMENTS')
+      expect(picture.comments.count).to eq(0)
+      within('#comment-form') do
+        fill_in('comment[body]', with: 'Test comment')
+        click_on('Add Comment')
+      end
+      expect(page).to have_content('1 COMMENT')
+      expect(picture.comments.count).to eq(1)
+
     end
   end
 end
