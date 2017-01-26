0a7548a2c81aa284e98e4e44ddec362b770e87aa
Vladimir Stepanov <vladimir.stepanov@faceit-team.com>
2017-01-26 11:20:59 +0200

    Fix request origins for Action Cable in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   config.action_cable.url = 'wss://cars-gallery.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = ['https://cars-gallery.herokuapp.com/', 'http://cars-gallery.herokuapp.com/']
+  config.action_cable.allowed_request_origins = ['https://cars-gallery.herokuapp.com', 'http://cars-gallery.herokuapp.com']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
