2b23d47b69a9047b97d78ee29ed7eb2a4656a20c
Vladimir Stepanov <vladimir.stepanov@faceit-team.com>
2017-02-09 17:53:13 +0200

    Add tests for Chat rooms

--- spec/features/chat_rooms_pages.rb
+++ spec/features/chat_rooms_pages.rb
@@ -7,6 +7,7 @@ RSpec.feature 'ChatRoom pages', :type => :feature do
 
   describe 'Index page' do
     before { 5.times { create(:chat_room, user: user) } }
+    let(:chat_room) { ChatRoom.first }
 
     context 'when user is not logged in' do
       before { visit chat_rooms_path }
@@ -33,13 +34,76 @@ RSpec.feature 'ChatRoom pages', :type => :feature do
         end
       end
     end
+
+    describe 'Click on New chat room', js: true do
+      before do
+        sign_in user
+        visit chat_rooms_path
+        click_on('New chat room')
+      end
+
+      it { should have_current_path(new_chat_room_path(locale: :en)) }
+      it { should have_content('Add chat room') }
+    end
+
+    describe 'Click on chat room', js: true do
+      before do
+        sign_in user
+        visit chat_rooms_path
+        click_on("#{chat_room.title}")
+      end
+
+      it { should have_current_path(chat_room_path(chat_room, locale: :en)) }
+      it { should have_content("#{chat_room.title}") }
+    end
   end
 
-  describe 'visit new chat room' do
+  describe 'Show Chat' do
+    let(:chat_room) { create(:chat_room, user: user) }
+    let(:message) { 'Hello my firend' }
 
+    context 'when user is not logged in' do
+      before { visit chat_room_path(chat_room) }
+
+      it { should have_css('div.alert.alert-danger') }
+      it { should have_content('You need to sign in or sign up before continuing.') }
+      it { should have_current_path(new_user_session_path(locale: :en)) }
+    end
+
+    context 'when user is logged in' do
+      before do
+        sign_in user
+        page.driver.options[:headers] ||= {}
+        page.driver.options[:headers]['REQUEST_PATH'] = chat_rooms_path
+        visit chat_room_path(chat_room)
+      end
+
+      it { should have_content("#{chat_room.title}") }
+      it { should have_button('Post') }
+    end
+
+    describe 'Post new message', js: true do
+      before do
+        sign_in user
+        visit chat_room_path(chat_room)
+      end
+
+      it 'User can post his message' do
+        expect(user.messages.count).to eq(0)
+        within('#new_message') do
+          fill_in('Your message', with: message)
+        end
+        click_button('Post')
+        expect(find_field(id: 'message_body').value).to eq('')
+        Capybara.using_wait_time 20 do
+          expect(page).to have_css('#messages p', text: message)
+          expect(user.messages.count).to eq(1)
+        end
+      end
+    end
   end
 
-  describe 'enter to a chat' do
+  describe 'Create New Chat' do
 
   end
 end
