da6552cb6fb0dd42fd37d18ef8fa6e208476ff1a
Vladimir Stepanov <vladimir.stepanov@faceit-team.com>
2017-02-09 18:11:35 +0200

    Add request origins for action cable test env

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -41,4 +41,6 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  config.action_cable.allowed_request_origins = [ 'http://localhost:3000', /http:\/\/*/, /https:\/\/*/ ]
 end
