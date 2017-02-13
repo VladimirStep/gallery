3268f15a982b990826440d6d718301fee7d4b2bd
Vladimir Stepanov <vladimir.stepanov@faceit-team.com>
2017-02-13 14:10:30 +0200

    Add localization tests

--- spec/features/index_page_spec.rb
+++ spec/features/index_page_spec.rb
@@ -24,6 +24,7 @@ RSpec.describe 'Index page', :type => :feature do
       it { should have_link('Events') }
       it { should have_link('Profile') }
       it { should have_link('Sign out') }
+      it { should have_content("#{user.email}") }
     end
   end
 
@@ -42,6 +43,35 @@ RSpec.describe 'Index page', :type => :feature do
       it { should have_no_link('Events') }
       it { should have_no_link('Profile') }
       it { should have_no_link('Sign out') }
+    end
+  end
+
+  describe 'localization' do
+    before do
+      sign_in user
+      page.driver.options[:headers] ||= {}
+      page.driver.options[:headers]['REQUEST_PATH'] = root_path
+      visit root_path
+    end
+
+    it 'should have Russian locale' do
+      click_link('ru')
+      expect(page).to have_content('Категории')
+      expect(page).to have_content('Картинки')
+      expect(page).to have_content('Чаты')
+      expect(page).to have_content('Комментарии')
+      expect(page).to have_content('Действия')
+      expect(page).to have_content('Профиль')
+    end
+
+    it 'should have English locale' do
+      click_link('en')
+      expect(page).to have_content('Categories')
+      expect(page).to have_content('Pictures')
+      expect(page).to have_content('Chat rooms')
+      expect(page).to have_content('Comments')
+      expect(page).to have_content('Events')
+      expect(page).to have_content('Profile')
     end
   end
 end
