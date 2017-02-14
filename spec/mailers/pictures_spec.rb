fda8f34f3e1199131acf9c81874d027c18e3c216
Vladimir Stepanov <vladimir.stepanov@faceit-team.com>
2017-02-14 11:33:59 +0200

    Add mailer tests

--- spec/mailers/pictures_spec.rb
+++ spec/mailers/pictures_spec.rb
@@ -1,5 +1,17 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe PicturesMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+  let(:picture) { create(:picture) }
+  let(:mail) { PicturesMailer.new_picture_email(user, picture) }
+
+  it 'renders the headers' do
+    expect(mail.subject).to eq('New picture for You')
+    expect(mail.to).to eq([user.email])
+    expect(mail.from).to eq(['from@example.com'])
+  end
+
+  it 'renders the body' do
+    expect(mail.body.encoded).to match('New Picture for You')
+  end
 end
